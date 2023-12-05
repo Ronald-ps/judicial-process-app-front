b6a1ab5563f95bdeed831c9e7e4ef811a3d10e2d
Ronald Seabra <git.ronaldseabra@gmail.com>
2023-12-05 01:45:59 -0300

    feat: estilo de botao de submit

--- src/components/client/ClientForm/index.tsx
+++ src/components/client/ClientForm/index.tsx
@@ -1,5 +1,6 @@
-import { clientMaritalStatus, educationLevel } from "@/services/client";
+import { Client, clientMaritalStatus, educationLevel } from "@/services/client";
 import {
+  Button,
   Flex,
   Group,
   NumberInput,
@@ -10,9 +11,11 @@ import {
 } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
 import { useForm } from "@mantine/form";
+import classes from "./ClientForm.module.css";
 
 interface ClientFormProps {
   title?: string;
+  client: Client;
 }
 export const ClientForm = (props: ClientFormProps) => {
   const form = useForm({
@@ -77,6 +80,11 @@ export const ClientForm = (props: ClientFormProps) => {
       },
     },
   });
+
+  if (props.client) {
+    form.setValues(props.client);
+  }
+
   return (
     <Stack>
       <Text size="xl" fw={600}>
@@ -84,130 +92,140 @@ export const ClientForm = (props: ClientFormProps) => {
       </Text>
 
       <form>
-        <Group w="100%" grow>
-          <TextInput
-            label="Nome"
-            required
-            withAsterisk
-            {...form.getInputProps("first_name")}
-          />
-          <TextInput
-            label="Sobrenome"
-            required
-            withAsterisk
-            {...form.getInputProps("last_name")}
-          />
-        </Group>
+        <Stack>
+          <Group w="100%" grow>
+            <TextInput
+              label="Nome"
+              required
+              withAsterisk
+              {...form.getInputProps("first_name")}
+            />
+            <TextInput
+              label="Sobrenome"
+              required
+              withAsterisk
+              {...form.getInputProps("last_name")}
+            />
+          </Group>
 
-        <Group grow w="100%">
-          <TextInput
-            label="Endereço"
-            required
-            withAsterisk
-            {...form.getInputProps("address")}
-          />
-          <TextInput
-            label="Cidade"
-            required
-            withAsterisk
-            {...form.getInputProps("city")}
-          />
-        </Group>
+          <Group grow w="100%">
+            <TextInput
+              label="Endereço"
+              required
+              withAsterisk
+              {...form.getInputProps("address")}
+            />
+            <TextInput
+              label="Cidade"
+              required
+              withAsterisk
+              {...form.getInputProps("city")}
+            />
+          </Group>
 
-        <Group grow w="100%">
-          <TextInput
-            label="CPF"
-            required
-            withAsterisk
-            {...form.getInputProps("cpf")}
-          />
-          <TextInput
-            label="RG"
-            required
-            withAsterisk
-            {...form.getInputProps("rg")}
-          />
-        </Group>
+          <Group grow w="100%">
+            <TextInput
+              label="CPF"
+              required
+              withAsterisk
+              {...form.getInputProps("cpf")}
+            />
+            <TextInput
+              label="RG"
+              required
+              withAsterisk
+              {...form.getInputProps("rg")}
+            />
+          </Group>
 
-        <Group grow w="100%">
-          <DateInput
-            label="Data de Nascimento"
-            required
-            withAsterisk
-            defaultLevel="decade"
-            locale="pt-BR"
-            {...form.getInputProps("birth_date")}
-            maxDate={new Date()}
-          />
-          <TextInput
-            label="Email"
-            required
-            withAsterisk
-            {...form.getInputProps("email")}
-          />
-        </Group>
+          <Group grow w="100%">
+            <DateInput
+              label="Data de Nascimento"
+              required
+              withAsterisk
+              defaultLevel="decade"
+              locale="pt-BR"
+              {...form.getInputProps("birth_date")}
+              maxDate={new Date()}
+            />
+            <TextInput
+              label="Email"
+              required
+              withAsterisk
+              {...form.getInputProps("email")}
+            />
+          </Group>
 
-        <Group grow>
-          <TextInput
-            label="Telefone"
-            required
-            withAsterisk
-            {...form.getInputProps("phone")}
-          />
-          <TextInput
-            label="Celular"
-            required
-            withAsterisk
-            {...form.getInputProps("cellphone")}
-          />
-        </Group>
+          <Group grow>
+            <TextInput
+              label="Telefone"
+              required
+              withAsterisk
+              {...form.getInputProps("phone")}
+            />
+            <TextInput
+              label="Celular"
+              required
+              withAsterisk
+              {...form.getInputProps("cellphone")}
+            />
+          </Group>
 
-        <Group grow>
-          <TextInput
-            label="Nome do Pai"
-            withAsterisk
-            {...form.getInputProps("father_name")}
-          />
-          <TextInput
-            label="Nome da Mãe"
-            required
-            withAsterisk
-            {...form.getInputProps("mother_name")}
-          />
-        </Group>
+          <Group grow>
+            <TextInput
+              label="Nome do Pai"
+              withAsterisk
+              {...form.getInputProps("father_name")}
+            />
+            <TextInput
+              label="Nome da Mãe"
+              required
+              withAsterisk
+              {...form.getInputProps("mother_name")}
+            />
+          </Group>
 
-        <Group grow>
-          <Select
-            label="Escolaridade"
-            required
-            withAsterisk
-            {...form.getInputProps("education_level")}
-            data={Object.values(educationLevel)}
-          />
-          <TextInput
-            label="Profissão"
-            required
-            withAsterisk
-            {...form.getInputProps("profession")}
-          />
-        </Group>
+          <Group grow>
+            <Select
+              label="Escolaridade"
+              required
+              withAsterisk
+              {...form.getInputProps("education_level")}
+              data={Object.values(educationLevel)}
+            />
+            <TextInput
+              label="Profissão"
+              required
+              withAsterisk
+              {...form.getInputProps("profession")}
+            />
+          </Group>
 
-        <Group grow>
-          <Select
-            label="Estado Civil"
-            placeholder="Selecione uma opção"
-            data={Object.values(clientMaritalStatus)}
-            required
-            withAsterisk
-            {...form.getInputProps("marital_status")}
-          />
-          <NumberInput
-            label="Quantidade de filhos"
-            required
-            withAsterisk
-            {...form.getInputProps("childrens_quantity")}
-          />
-        </Group>
+          <Group grow>
+            <Select
+              label="Estado Civil"
+              placeholder="Selecione uma opção"
+              data={Object.values(clientMaritalStatus)}
+              required
+              withAsterisk
+              {...form.getInputProps("marital_status")}
+            />
+            <NumberInput
+              label="Quantidade de filhos"
+              required
+              withAsterisk
+              {...form.getInputProps("childrens_quantity")}
+            />
+          </Group>
+          <Button
+            w="25%"
+            type="submit"
+            color="#1c16428c"
+            className={classes.clientFormSubmitButton}
+          >
+            Salvar
+          </Button>
+        </Stack>
       </form>
     </Stack>
   );
@@ -224,7 +242,9 @@ Filhos:Filhos: ( ) Sim ( ) Não Quantos:Quantos: _______________________________
 Escolaridade:Escolaridade: ____________________________________ Profissão:Profissão: ____________________________________
 Estado Cívil:Estado Cívil: ____________________________ Cidade:Cidade: ______________________________________________ */
 
-/*   id: number;
+/*
+  client ->
+   id: number;
   first_name: string;
   last_name: string;
   cpf: string;
