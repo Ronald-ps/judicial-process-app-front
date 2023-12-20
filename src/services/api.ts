d1ec1e3eb2fd1de241317802246a363799af2f89
Ronald Seabra <git.ronaldseabra@gmail.com>
2023-12-19 22:22:10 -0300

    feat: serviço para busca de token

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,9 +3,97 @@ import axios from "axios";
 const baseUrl: string | undefined = import.meta.env.VITE_BACKEND_URL; /* .env */
 if (!baseUrl) throw new Error("VITE_BACKEND_URL is not defined");
 
-export const defaultClient = axios.create({
+const defaultClient = axios.create({
   baseURL: baseUrl,
   timeout: 60000,
   withCredentials: true,
 });
 axios.defaults.headers.common["Content-Type"] = "application/json";
+
+class TokenService {
+  public token: string | null = null;
+  private expireTime: Date | null = null;
+  private refreshToken: string | null = null;
+
+  verifyCurrentToken(): boolean {
+    if (!this.token) return false;
+    if (!this.expireTime) return false;
+    const actualDate = new Date();
+    return actualDate < this.expireTime;
+  }
+
+  getCurrentToken(): string | null {
+    console.log(this);
+    if (!this.token) return null;
+    if (this.verifyCurrentToken()) return this.token;
+    this.updateCurrentToken();
+    let tokenIsUpdated = false;
+    while (!tokenIsUpdated) {
+      if (this.verifyCurrentToken()) {
+        tokenIsUpdated = true;
+      }
+    }
+    return this.token;
+  }
+
+  async getRefreshToken() {
+    if (this.refreshToken) return this.refreshToken;
+    const refreshToken = localStorage.getItem("refresh_token");
+    this.refreshToken = refreshToken;
+    return refreshToken;
+  }
+
+  async updateCurrentToken() {
+    defaultClient
+      .get("/api/token/refresh", {
+        params: { refresh: this.getRefreshToken() },
+      })
+      .then((r) => {
+        this.token = r.data.access;
+        const actualDate = new Date();
+        this.expireTime = new Date(actualDate.getTime() + 5 * 60000);
+      });
+  }
+
+  setCurrentToken(token: string) {
+    this.token = token;
+    const actualDate = new Date();
+    this.expireTime = new Date(actualDate.getTime() + 5 * 60000);
+  }
+
+  saveRefreshToken(refreshToken: string) {
+    localStorage.setItem("refresh_token", refreshToken);
+    this.refreshToken = refreshToken;
+  }
+
+  async setRefreshToken(email: string, password: string) {
+    try {
+      const { refresh, access } = await defaultClient
+        .post("api/token/", { email, password })
+        .then((r) => r.data);
+      console.log(refresh, access);
+
+      this.saveRefreshToken(refresh);
+      this.setCurrentToken(access);
+    } catch (err) {
+      throw new Error("Erro de validação");
+    }
+  }
+}
+
+const tokenService = new TokenService();
+
+defaultClient.interceptors.request.use(
+  (config) => {
+    const token = tokenService.getCurrentToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { defaultClient, tokenService as DefaultClientTokenService };
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,14 +1,19 @@
-import { defaultClient } from "@services/api"
+import { defaultClient, DefaultClientTokenService } from "@services/api";
 
 export interface LoggedUser {
-  id: number
-  email: string
-  name: string
+  id: number;
+  email: string;
+  name: string;
 }
 export const getLoggedUser = (): Promise<LoggedUser> => {
-  return defaultClient.get("/whoami").then(r => r.data)
-}
+  return defaultClient.get("/whoami").then((r) => r.data);
+};
 
-export const login = (email: string, password: string): Promise<LoggedUser> => {
-  return defaultClient.post("/login", { email, password }).then(r => r.data)
-}
+export const login = async (
+  email: string,
+  password: string
+): Promise<LoggedUser> => {
+  await DefaultClientTokenService.setRefreshToken(email, password);
+  const loggedUser = await getLoggedUser();
+  return loggedUser;
+};
