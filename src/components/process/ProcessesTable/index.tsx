dfebd96da9e878feaee7030bf13061cdefb9381a
Ronald Seabra <git.ronaldseabra@gmail.com>
2023-12-26 23:21:12 -0300

    feat: tabela de processos com scroll

--- src/components/process/ProcessesTable/index.tsx
+++ src/components/process/ProcessesTable/index.tsx
@@ -6,43 +6,47 @@ import { ROUTER_PATHS } from "@/pages/routers";
 
 interface ProcessesTableProps {
   processes: DetailedProcess[];
+  maxHeight?: string | number;
 }
 export const ProcessesTable = (props: ProcessesTableProps) => {
   const navigate = useNavigateWithConstructRoute();
 
   return (
-    <Table striped>
-      <Table.Thead>
-        <Table.Tr>
-          <Table.Th>Nº</Table.Th>
-          <Table.Th>Data de início</Table.Th>
-          <Table.Th>Serviço</Table.Th>
-        </Table.Tr>
-      </Table.Thead>
-
-      <Table.Tbody>
-        {props.processes.map((process, i) => (
-          <Table.Tr
-            key={i}
-            onClick={() => {
-              navigate({
-                routerPath: ROUTER_PATHS.PROCESS_DETAIL,
-                params: { processId: process.id },
-              });
-            }}
-            style={{ cursor: "pointer" }}
-          >
-            <Table.Td>{process.code}</Table.Td>
-            <Table.Td>{formatDate({ date: process.start_date })}</Table.Td>
-            <Table.Td>{process.type}</Table.Td>
+    <Table.ScrollContainer minWidth={500} h={props.maxHeight ?? 430}>
+      <Table striped stickyHeader>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Nº</Table.Th>
+            <Table.Th>Data de início</Table.Th>
+            <Table.Th>Serviço</Table.Th>
           </Table.Tr>
-        ))}
-      </Table.Tbody>
-      {props.processes.length === 0 && (
-        <Table.Caption>
-          <Text size="xl">Nenhum Processo</Text>
-        </Table.Caption>
-      )}
-    </Table>
+        </Table.Thead>
+
+        <Table.Tbody>
+          {props.processes.map((process, i) => (
+            <Table.Tr
+              key={i}
+              onClick={() => {
+                navigate({
+                  routerPath: ROUTER_PATHS.PROCESS_DETAIL,
+                  params: { processId: process.id },
+                });
+              }}
+              style={{ cursor: "pointer" }}
+            >
+              <Table.Td>{process.code}</Table.Td>
+              <Table.Td>{formatDate({ date: process.start_date })}</Table.Td>
+              <Table.Td>{process.type}</Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+
+        {props.processes.length === 0 && (
+          <Table.Caption>
+            <Text size="xl">Nenhum Processo</Text>
+          </Table.Caption>
+        )}
+      </Table>
+    </Table.ScrollContainer>
   );
 };
