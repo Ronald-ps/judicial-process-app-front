e5a11256b9310f4a27f38523dea5804cfa3b55e4
Ronald Seabra <ronald.seabra@itmss.com.br>
2024-01-17 03:19:25 -0300

    fix: vite.config.ts base para deply github

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pluralcrm/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
