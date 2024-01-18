4e3def111edd4caffa623b7173e5e6abad4ed99f
Ronald Seabra <ronald.seabra@itmss.com.br>
2024-01-18 10:23:00 -0300

    feat: usar base url vindo de enviroment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: import.meta.env?.VITE_FRONT_BASE_URL || '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
